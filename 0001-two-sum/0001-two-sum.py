c206fc9512155d30eb9b29f75c348be0ed00c47b
subasri-25 <subashankar2004@gmail.com>
2025-09-30 20:12:24 +0530

    Time: 2035 ms (36.64%), Space: 13.4 MB (16.12%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -5,9 +5,17 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        for i in range(len(nums)):
-            for j in range(i, len(nums)):
+        for i in range(len(nums)): # Loops through every index i in the list.
+            for j in range(i, len(nums)): # This compares each element nums[i] with every other element nums[j].
                 if nums[i] + nums[j] == target and i != j:
                     return i, j
+
+        #  dic = {}  # initialise an empty dictionary
+
+        # for i, val in enumerate(nums):
+        #     if target - val in dic:
+        #         return [dic[target - val], i]
+        #     else:
+        #         dic[val] = i
     
         
