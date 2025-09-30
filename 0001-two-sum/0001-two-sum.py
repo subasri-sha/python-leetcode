5ce5c20e21017921fc4e02fc0bb7d78769c04a4b
subasri-25 <subashankar2004@gmail.com>
2025-09-30 20:14:06 +0530

    Time: 2040 ms (34.72%), Space: 13.3 MB (31.1%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -9,7 +9,7 @@ class Solution(object):
             for j in range(i, len(nums)): # This compares each element nums[i] with every other element nums[j].
                 if nums[i] + nums[j] == target and i != j:
                     return i, j
-
+        # Alternative code for better time complexity
         #  dic = {}  # initialise an empty dictionary
 
         # for i, val in enumerate(nums):
